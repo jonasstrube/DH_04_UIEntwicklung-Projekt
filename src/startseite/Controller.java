e858d412ecf74df9a2b5553100c397e9877b4bb8
Zarpator <strube.jonas@gmail.com>
2019-03-19 19:32:23 +0100

    fix package declaration in startseite files

--- src/startseite/Controller.java
+++ src/startseite/Controller.java
@@ -1,4 +1,4 @@
-package application;
+package startseite;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
--- src/startseite/Main.java
+++ src/startseite/Main.java
@@ -1,4 +1,4 @@
-package application;
+package startseite;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;

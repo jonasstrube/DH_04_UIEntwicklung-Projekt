9f82b79313cbd6587b73628c59a6d30e3db5e389
UnderTheCraft <yannick.ulmrich@sap.com>
2019-04-05 10:22:29 +0200

    Added News and Tiles for Startseite

--- src/data/DataProvider.java
+++ src/data/DataProvider.java
@@ -1,10 +1,12 @@
 package data;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import kalender.main.Veranstaltung;
 import notenuebersicht.Modul;
+import startseite.Tile;
 
 public class DataProvider {
 	private static List<Modul> semester1;
@@ -65,5 +67,20 @@ public class DataProvider {
 		
 		return noten;
 	}
+
+	private static Collection<Tile> getNews() {
+        Collection<Tile> news = new ArrayList<Tile>();
+        news.add(new Tile("Heute:","Die Vorlesung Rechnungslegung fällt morgen aus!"));
+        news.add(new Tile("18.02","Die Vorlesung Verteilte Systeme fällt am 19.2 aus!"));
+        news.add(new Tile("02.02","Die Abgabefirst für die UI-DOku ist der 05.04!"));
+        news.add(new Tile("28.01","Die Vorlesung Datenbanken wird um 1h auf 15 Uhr verschoben!"));
+        return news;
+    }
+    private static Collection<Tile> getTiles(){
+        Collection<Tile> tiles = new ArrayList<Tile>();
+        tiles.add(new Tile("Notenübersicht","Neuste Note: 1,3 in Statistik"));
+        tiles.add(new Tile("Stundenplan","Nächste Vorlesung: UI in 2h 34min"));
+        return tiles;
+    }
 	
 }
